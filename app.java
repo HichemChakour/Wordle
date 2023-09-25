0248e7658739217186baa186219364eb7aa688a9
hichem chakour <hchakour2003@gmail.com>
2023-09-25 14:40:35 +0000

    debut de l'application

---
+++ app.java
@@ -0,0 +1,20 @@
+package wordle;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class app extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("NEW WORDLE");
+		primaryStage.setMaximized(true);
+		//primaryStage.setResizable(false);
+		primaryStage.show();
+		primaryStage.centerOnScreen();
+		
+		
+		
+	}
+
+}
