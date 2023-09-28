af7d1fe3cb40a6f771550bc61036fabc6062b839
ylies Chementel <ylies.chementel@alumni.univ-avignon.fr>
2023-09-28 13:01:38 +0000

    Update Parser.java by adding comments to the Parser class to describe its fields and methods, by cleaning the code by replacing the names of functions and variables with proper names and by adding a new function called removeAccent()

--- Parser.java
+++ Parser.java
@@ -1,141 +1,132 @@
 package Game;
 
-import org.jsoup.Jsoup; 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * This class provides methods for parsing and processing words from web
+ * sources.
+ * 
+ * @author Chementel Ylies
+ */
 
 public class Parser {
 
-	public static void main(String[] args) {
-		Parser dico = new Parser();
-		dico.dico();
+	/**
+	 * Removes accents from a given word.
+	 * 
+	 * @param word The word with accents.
+	 * @return The word with accents removed.
+	 */
+	public String removeAccent(String word) {
+		if (word.contains("à") || word.contains("á") || word.contains("â") || word.contains("ä")) {
+			word = word.replace("à", "a").replace("á", "a").replace("â", "a").replace("ä", "a");
+		}
+		if (word.contains("é") || word.contains("è") || word.contains("ê") || word.contains("ë")) {
+			word = word.replace("é", "e").replace("è", "e").replace("ê", "e").replace("ë", "e");
+		}
+		if (word.contains("î") || word.contains("ï")) {
+			word = word.replace("î", "i").replace("ï", "i");
+		}
+		if (word.contains("ô") || word.contains("ö")) {
+			word = word.replace("ô", "o").replace("ö", "o");
+		}
+		if (word.contains("û") || word.contains("ù") || word.contains("ü")) {
+			word = word.replace("û", "u").replace("ù", "u").replace("ü", "u");
+		}
+		if (word.contains("ç")) {
+			word = word.replace("ç", "c");
+		}
+		if (word.contains("œ")) {
+			word = word.replace("œ", "oe");
+		}
+		if (word.contains("æ")) {
+			word = word.replace("æ", "ae");
+		}
+		return word;
 	}
-	
-	public void fileWords() {
-		String url ="https://fr.wiktionary.org/wiki/Wiktionnaire:Liste_de_1750_mots_fran%C3%A7ais_les_plus_courants";
-			try {
-	            Document recup = Jsoup.connect(url).get();
-	            Elements tags = recup.select("a");
-	            String[] mots = new String[tags.size()]; 
-	            int index = 0;
-	            for (Element tag : tags) {
-	                if (!tag.select("*").isEmpty()) {
-		                String mot = tag.attr("title");
-		                if (mot != null && !mot.contains(" ") && !mot.contains("-")) {
-		                    if (mot.contains("à") || mot.contains("á") || mot.contains("â") || mot.contains("ä")) {
-		                        mot = mot.replace("à", "a").replace("á", "a").replace("â", "a").replace("ä", "a");
-		                    }
-		                    if (mot.contains("é") || mot.contains("è") || mot.contains("ê") || mot.contains("ë")) {
-		                        mot = mot.replace("é", "e").replace("è", "e").replace("ê", "e").replace("ë", "e");
-		                    }
-		                    if (mot.contains("î") || mot.contains("ï")) {
-		                        mot = mot.replace("î", "i").replace("ï", "i");
-		                    }
-		                    if (mot.contains("ô") || mot.contains("ö")) {
-		                        mot = mot.replace("ô", "o").replace("ö", "o");
-		                    }
-		                    if (mot.contains("û") || mot.contains("ù") || mot.contains("ü")) {
-		                        mot = mot.replace("û", "u").replace("ù", "u").replace("ü", "u");
-		                    }
-		                    if (mot.contains("ç")) {
-		                        mot = mot.replace("ç", "c");
-		                    }
-		                    if (mot.contains("œ")) {
-		                        mot = mot.replace("œ", "oe");
-		                    }
-		                    if (mot.contains("æ")) {
-		                        mot = mot.replace("æ", "ae");
-		                    }
-		                    mots[index++] = mot.toUpperCase();
-		                }
-	                }
-	            }
 
-	            for (int i = 2; i <= 12; i++) {
-	            	String dossier = "Lettres_files/";
-	                String nomFichier = dossier + i + "_Lettres.txt";
-	                FileWriter writer = new FileWriter(nomFichier,true);
+	/**
+	 * Scrapes a web page to extract common French words and store them in files
+	 * based on word length.
+	 */
+	public void gameWords() {
+		String url = "https://fr.wiktionary.org/wiki/Wiktionnaire:Liste_de_1750_mots_fran%C3%A7ais_les_plus_courants";
+		try {
+			Document recovery = Jsoup.connect(url).get();
+			Elements tags = recovery.select("a");
+			String[] words = new String[tags.size()];
+			int index = 0;
+			for (Element tag : tags) {
+				if (!tag.select("*").isEmpty()) {
+					String word = tag.attr("title");
+					if (word != null && !word.contains(" ") && !word.contains("-")) {
+						words[index++] = removeAccent(word).toUpperCase();
+					}
+				}
+			}
 
-	                for (String mot : mots) {
-	                    if (mot != null && mot.length() == i) {
-	                        writer.write(mot + "\n");
-	                    }
-	                }
+			for (int i = 2; i <= 12; i++) {
+				String directory = "Game_Words_files/";
+				String fileName = directory + "Words_with_" + i + "_letters.txt";
+				FileWriter writer = new FileWriter(fileName, true);
 
-	                writer.close();
-	            }
+				for (String word : words) {
+					if (word != null && word.length() == i) {
+						writer.write(word + "\n");
+					}
+				}
 
-	            System.out.println("File created");
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
+				writer.close();
+				System.out.println("Game words file created");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	public void dico() {
-		String dossier = "dico_files/";
+
+	/**
+	 * Scrapes a web page to extract dictionary words and store them in files based
+	 * on word length.
+	 */
+	public void dictionaryWords() {
+		String directory = "Dictionary_Words_files/";
 		for (int code = 97; code <= 122; code++) {
-			String url ="https://usito.usherbrooke.ca/index/mots/tous/"+(char)code+"#"+(char)code;
-			System.out.println(url);
+			String url = "https://usito.usherbrooke.ca/index/mots/tous/" + (char) code + "#" + (char) code;
 			try {
-			    Document recup = Jsoup.connect(url).get();
-		        Elements tags = recup.select("a");
-		        String[] mots = new String[tags.size()];
-		        int index = 0;
-		        for (Element tag : tags) {
-		             if (!tag.select("*").isEmpty()) {
-			                String mot = tag.text();
-			                if (mot != null && !mot.contains(" ") && !mot.contains("-")) {
-			                	 if (mot.contains("à") || mot.contains("á") || mot.contains("â") || mot.contains("ä")) {
-				                        mot = mot.replace("à", "a").replace("á", "a").replace("â", "a").replace("ä", "a");
-			                    }
-			                    if (mot.contains("é") || mot.contains("è") || mot.contains("ê") || mot.contains("ë")) {
-			                        mot = mot.replace("é", "e").replace("è", "e").replace("ê", "e").replace("ë", "e");
-			                    }
-			                    if (mot.contains("î") || mot.contains("ï")) {
-			                        mot = mot.replace("î", "i").replace("ï", "i");
-			                    }
-			                    if (mot.contains("ô") || mot.contains("ö")) {
-			                        mot = mot.replace("ô", "o").replace("ö", "o");
-			                    }
-			                    if (mot.contains("û") || mot.contains("ù") || mot.contains("ü")) {
-			                        mot = mot.replace("û", "u").replace("ù", "u").replace("ü", "u");
-			                    }
-			                    if (mot.contains("ç")) {
-			                        mot = mot.replace("ç", "c");
-			                    }
-			                    if (mot.contains("œ")) {
-			                        mot = mot.replace("œ", "oe");
-			                    }
-			                    if (mot.contains("æ")) {
-			                        mot = mot.replace("æ", "ae");
-			                    }
-			                    mots[index++] = mot.toUpperCase();
-			                }
-		             }
-		         }
-		
-		        for (int i = 2; i <= 12; i++) {
-		             String nomFichier = dossier + i +"_Lettres.txt";
-		             FileWriter writer = new FileWriter(nomFichier,true);
-		
-		             for (String mot : mots) {
-		                 if (mot != null && mot.length() == i) {
-		                     writer.write(mot + "\n");
-		                 }
-		             }
-		
-		             writer.close();
-		         }
-		
-		         //System.out.println("File created");
-		    } 
-			catch (IOException e) {
-	         e.printStackTrace();
+				Document recovery = Jsoup.connect(url).get();
+				Elements tags = recovery.select("a");
+				String[] words = new String[tags.size()];
+				int index = 0;
+				for (Element tag : tags) {
+					if (!tag.select("*").isEmpty()) {
+						String word = tag.text();
+						if (word != null && !word.contains(" ") && !word.contains("-")) {
+							words[index++] = removeAccent(word).toUpperCase();
+						}
+					}
+				}
+
+				for (int i = 2; i <= 12; i++) {
+					String fileName = directory + "Words_with_" + i + "_letters.txt";
+					FileWriter writer = new FileWriter(fileName, true);
+
+					for (String word : words) {
+						if (word != null && word.length() == i) {
+							writer.write(word + "\n");
+						}
+					}
+					writer.close();
+				}
+				System.out.println("Dictionary words file updated");
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }
