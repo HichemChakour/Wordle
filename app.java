fe0cf0ade2b03cba622054e6e07034dae405125e
hichem chakour <hchakour2003@gmail.com>
2023-10-03 13:34:04 +0000

    update de l'app

--- app.java
+++ app.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -14,66 +15,49 @@ import javafx.scene.image.ImageView;
 
 public class app extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("NEW WORDLE");//titre de l'app
-		
-		StackPane root = new StackPane();
-		VBox BOX = new VBox(10);//stack les elements a l'ecran avec le parents du noeud root
-		
-		TextField Ligne1 = new TextField();
-		Ligne1.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne1.setText(newText.substring(0, 1));
-	            }
-	        });
-		TextField Ligne2 = new TextField();
-		Ligne2.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne2.setText(newText.substring(0, 1));
-	            }
-	        });
-		TextField Ligne3 = new TextField();
-		Ligne3.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne3.setText(newText.substring(0, 1));
-	            }
-	        });
-		TextField Ligne4 = new TextField();
-		Ligne4.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne4.setText(newText.substring(0, 1));
-	            }
-	        });
-		TextField Ligne5 = new TextField();
-		Ligne5.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne5.setText(newText.substring(0, 1));
-	            }
-	        });
-		TextField Ligne6 = new TextField();
-		Ligne6.textProperty().addListener((obs, oldText, newText) -> {
-	            if (newText.length() > 1) {
-	            	Ligne6.setText(newText.substring(0, 1));
-	            }
-	        });
-		Image fond = new Image("file:FOND.jpg");
-		ImageView fondView = new ImageView(fond);
-		fondView.fitWidthProperty().bind(primaryStage.widthProperty());
-		fondView.fitHeightProperty().bind(primaryStage.heightProperty());
-		Label Phrase =new Label("WORDLE",Color.WHITE);
-		root.getChildren().addAll(fondView,BOX);
-		
-		BOX.getChildren().addAll(Phrase,Ligne1,Ligne2,Ligne3,Ligne4,Ligne5,Ligne6);
-		Scene scene = new Scene(root, 600, 400);//la premiere scean parent
-		primaryStage.setScene(scene);//afficher la scene
-		primaryStage.setMaximized(true);//ouvrir la fenetre au max
-		//primaryStage.setResizable(false);//ne pas modifier la taille de fenetre
-		primaryStage.show();//afficher la fenetre
-		primaryStage.centerOnScreen();//mettre la fenetre au centre
-		
-		
-		
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("NEW WORDLE"); // Titre de l'application
 
+        int nbligne = 6;
+        int nblettre = 5;
+        StackPane root = new StackPane();
+        VBox box = new VBox(10); // Empile les éléments à l'écran avec le parent du nœud root
+        HBox[] ligne = new HBox[nbligne];
+        TextField[][] lettre = new TextField[nbligne][nblettre];
+
+        for (int i = 0; i < nbligne; i++) {
+            ligne[i] = new HBox(10); // Crée un HBox pour chaque ligne
+            for (int j = 0; j < nblettre; j++) {
+                lettre[i][j] = new TextField(); // Crée un champ de texte
+                final int row = i;
+                final int col = j;
+
+                lettre[i][j].textProperty().addListener((obs, oldText, newText) -> {
+                    if (newText.length() > 1) {
+                        lettre[row][col].setText(newText.substring(0, 1));
+                    }
+                });
+
+                ligne[i].getChildren().add(lettre[i][j]);
+            }
+            box.getChildren().add(ligne[i]); // Ajoute la ligne à la boîte
+        }
+
+        Image fond = new Image("file:FOND.jpg");
+        ImageView fondView = new ImageView(fond);
+        fondView.fitWidthProperty().bind(primaryStage.widthProperty());
+        fondView.fitHeightProperty().bind(primaryStage.heightProperty());
+        Label phrase = new Label("WORDLE");
+        root.getChildren().addAll(fondView, box);
+
+        Scene scene = new Scene(root, 600, 400); // La première scène parent
+        primaryStage.setScene(scene); // Afficher la scène
+        primaryStage.show(); // Afficher la fenêtre
+        primaryStage.centerOnScreen(); // Mettre la fenêtre au centre
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
